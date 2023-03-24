6916d0c84bd7311f844529cd2fb577edf40cd648
Takeshi <wattks@hotmail.com>
2023-03-24 11:26:40 -0300

    Exercício 2

--- Lista 1/Multimetro.java
+++ Lista 1/Multimetro.java
@@ -6,30 +6,31 @@ public class Multimetro{
     static final int NUM_AMOSTRAS = 10;
 
     public static void main(String[] args){
-        // Entrada de dados por teclado
-        Scanner scanner = new Scanner(System.in);
-        double[] volts = new double[NUM_AMOSTRAS];
-        for(int i=0; i<NUM_AMOSTRAS; i++){
-            System.out.print("Amostra " + (i+1) + ": ");
-            volts[i] = scanner.nextDouble();
+        try (Scanner scanner = new Scanner(System.in)){
+            // Entrada de dados por teclado
+            // double[] volts = new double[NUM_AMOSTRAS];
+            // for(int i=0; i<NUM_AMOSTRAS; i++){
+            //     System.out.print("Amostra " + (i+1) + ": ");
+            //     volts[i] = scanner.nextDouble();
+            // }
+            double[] volts = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            // Amostra de dados
+            System.out.print("\nAmosta de Volts: ");
+            for(double num : volts)
+                System.out.print(num + ", ");
+
+            // Cálculo da media
+            double media = calcularMedia(volts);
+            System.out.println("\nMedia: " + media);
+            
+            // Cálculo do desvio padrão
+            double desvioPadrao = calcularDesvioPadrao(volts, media);
+            System.out.println("Desvio Padrão: " + desvioPadrao);
+            
+            // Situação do multímetro
+            System.out.println("Situação: " + ((desvioPadrao > media*0.1) ? "Multímetro com problema." : "Multímetro OK."));
         }
-        // double[] volts = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-        // Amostra de dados
-        System.out.print("\nAmosta de Volts: ");
-        for(double num : volts)
-            System.out.print(num + ", ");
-
-        // Cálculo da media
-        double media = calcularMedia(volts);
-        System.out.println("\nMedia: " + media);
-        
-        // Cálculo do desvio padrão
-        double desvioPadrao = calcularDesvioPadrao(volts, media);
-        System.out.println("\nDesvio Padrão: " + desvioPadrao);
-        
-        // Situação do multímetro
-        System.out.println((desvioPadrao > media*0.1) ? "Multímetro com problemas." : "Multímetro OK.");
     }
 
     public static double calcularMedia(double[] amostras){
