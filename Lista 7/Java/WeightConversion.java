08a3db1a5cf8872dc4c4e450fc17916ea85a4202
Takeshi <wattks@hotmail.com>
2023-05-27 23:12:45 -0300

    .

---
+++ Lista 7/Java/WeightConversion.java
@@ -0,0 +1,85 @@
+public class WeightConversion {
+    /**
+     * Convert Grams to Kilograms
+     */
+    public static double g_kg(double g) {
+        return g * 0.001;
+    }
+
+    /**
+     * Convert Grams to Pounds
+     */
+    public static double g_lb(double g) {
+        return g * 0.00220462262185;
+    }
+
+    /**
+     * Convert Grams to Ounces
+     */
+    public static double g_oz(double g) {
+        return g * 0.03527396195;
+    }
+
+    /**
+     * Convert Kilograms to Grams
+     */
+    public static double kg_g(double kg) {
+        return kg * 1000;
+    }
+
+    /**
+     * Convert Kilograms to Pounds
+     */
+    public static double kg_lb(double kg) {
+        return kg * 2.20462262185;
+    }
+    
+    /**
+     * Convert Kilograms to Ounces
+     */
+    public static double kg_oz(double kg) {
+        return kg * 35.27396195;
+    }
+
+    /**
+     * Convert Pounds to Grams
+     */
+    public static double lb_g(double lb) {
+        return lb * 453.59237;
+    }
+
+    /**
+     * Convert Pounds to Kilograms
+     */
+    public static double lb_kg(double lb) {
+        return lb *  0.45359237;
+    }
+
+    /**
+     * Convert Pounds to Ounces
+     */
+    public static double lb_oz(double lb) {
+        return lb * 16;
+    }
+
+    /**
+     * Convert Ounces to Grams
+     */
+    public static double oz_g(double oz) {
+        return oz * 28.34952;
+    }
+
+    /**
+     * Convert Ounces to Kilograms
+     */
+    public static double oz_kg(double oz) {
+        return oz * 0.02834952;
+    }
+
+    /**
+     * Convert Ounces to Pounds
+     */
+    public static double oz_lb(double oz) {
+        return oz * 0.0625;
+    }
+}
---
+++ Lista 7/Java/WeightConversionExample.java
@@ -0,0 +1,5 @@
+public class WeightConversionExample {
+    public static void main(String[] args) {
+        System.out.println(WeightConversion.g_kg(1000));
+    }
+}
